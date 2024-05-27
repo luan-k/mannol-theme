c52f36898bb7fc90268a8fcf9c10747349379bab
Luan <luankds@gmail.com>
2024-05-27 17:59:44 -0300

    chore: Refactor styles for consistent padding and margin

--- single.php
+++ single.php
@@ -3,12 +3,6 @@
 // Get the current post ID
 $post_id = get_the_ID();
 
-// Get the categories for this post
-$categories = get_the_category($post_id);
-
-// Get the first category
-$category = $categories[0];
-
 // Get the permalink
 $permalink = get_permalink($post_id);
 
@@ -31,14 +25,31 @@ if ($post_thumbnail_url) {
 // Get the content
 $content = get_the_content(null, false, $post_id);
 
-// Get related posts
-$args = array(
-    'category__in' => array($category->term_id),
-    'post__not_in' => array($post_id),
-    'posts_per_page' => 5, // Number of related posts to display.
-);
+// Get the categories for this post
+$categories = get_the_terms($post_id, 'category');
 
-$related_posts = get_posts($args);
+// Ensure there are categories
+if ($categories && !is_wp_error($categories)) {
+    // Get the first category
+    $category = $categories[0];
+
+    // Get related posts
+    $args = array(
+        'post_type' => 'produtos', // Ensure you query the custom post type
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'category',
+                'field'    => 'term_id',
+                'terms'    => $category->term_id,
+            ),
+        ),
+        'post__not_in' => array($post_id),
+        'posts_per_page' => 4, // Number of related posts to display
+    );
+
+    $related_posts = new WP_Query($args);
+    wp_reset_postdata();
+}
 
 $trail = "Produtos / {$category->slug} / {$title}";
 
@@ -58,25 +69,31 @@ wp_reset_postdata();
             </h1>
         </div>
     </div>
-    <div class="single-wk-products__image">
+    <div class="single-wk-products__image py-12">
        <div class="container">
             <img class="m-auto w-1/3 h-auto" src="<?php echo $post_thumbnail; ?>" alt="<?php echo $title; ?>">
        </div>
     </div>
     <div class="single-wk-products__text">
-        <?php echo $content; ?>
+        <div class="container">
+            <?php echo $content; ?>
+        </div>
     </div>
-    <div class="single-wk-products__related">
-        <h2>Produtos Relacionados</h2>
-        <div class="grid grid-cols-3 gap-10">
-            <?php foreach ($related_posts as $post) : setup_postdata($post); ?>
-                <div class="single-wk-products__related-item">
-                    <a href="<?php the_permalink(); ?>">
-                        <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
-                        <h3><?php the_title(); ?></h3>
-                    </a>
-                </div>
-            <?php endforeach; ?>
+     <div class="single-wk-products__related py-12">
+        <h2 class="text-center">Produtos Relacionados</h2>
+        <div class="container grid grid-cols-4 gap-10 py-12">
+            <?php if ($related_posts->have_posts()) : ?>
+                <?php while ($related_posts->have_posts()) : $related_posts->the_post(); ?>
+                    <div class="single-wk-products__related-item">
+                        <a href="<?php the_permalink(); ?>">
+                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+                            <h3><?php the_title(); ?></h3>
+                        </a>
+                    </div>
+                <?php endwhile; ?>
+            <?php else : ?>
+                <p class="text-center">No related products found.</p>
+            <?php endif; ?>
         </div>
     </div>
  </section>
