19c3ebc7d825d6075bb50afd6b3e4ffd213d1c3d
Luan <luankds@gmail.com>
2024-06-01 22:22:33 -0300

    Refactor header.php to fix PHP opening tag and add missing semicolon

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <? wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?> >
